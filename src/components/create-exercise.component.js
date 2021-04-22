523b51d0b543ece470557eff7d91dff36dc572a0
Ross Koeberl <rtkoeberl@gmail.com>
2021-04-22 13:13:15 -0500

    Finished frontend build

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -6,6 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
+    
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangeDescription = this.onChangeDescription.bind(this);
     this.onChangeDuration = this.onChangeDuration.bind(this);
